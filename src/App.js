85555e2e32eda6d84d2f0b211988821bcc7f7a72
Lei Marie Lafaele <llafaele.murphy@outlook.com>
2022-06-07 15:01:31 -0400

    Updated live degrees F

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import './App.css';
 
 function App() {
 
-  const [weather, setWeather] = useState([])
+  const [weather, setWeather] = useState("")
 
   const getWeather = () => {
 Axios.get('https://api.weatherapi.com/v1/current.json?key=95fdf37b8f2e4a2a8e2171932220306&q=Massachusetts&aqi=yes').then((response)=> {
-  console.log(response.data.location);
-  setWeather(response.data.location)
+  console.log(response.data.location.name + ", " + response.data.location.region);
+  setWeather('Temperature in ' + response.data.location.name + ", " + response.data.location.region + ' is ' + response.data.current.temp_f + ' degrees Farenheit')
     }
   );
 };
