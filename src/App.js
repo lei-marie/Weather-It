c5d4cefd59d9a96d7c95ee4bef7c370bacae37c7
Lei Marie Lafaele <llafaele.murphy@outlook.com>
2022-06-07 14:50:49 -0400

    Retrieved data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
+import Axios from 'axios'
 import './App.css';
 
 function App() {
-  return <div>What's the weather today?</div>
+
+  const [weather, setWeather] = useState([])
+
+  const getWeather = () => {
+Axios.get('https://api.weatherapi.com/v1/current.json?key=95fdf37b8f2e4a2a8e2171932220306&q=Massachusetts&aqi=yes').then((response)=> {
+  console.log(response.data.location);
+  setWeather(response.data.location)
+    }
+  );
+};
+  return (
+    <div>
+      What's the weather today? <button onClick={getWeather}> Weather Now </button>
+      {weather}
+  </div>
+  );
 }
 
 export default App;
